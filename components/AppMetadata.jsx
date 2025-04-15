b7435cb3f9f92bc43c078f2a77f3755acb2a7910
bhavesh132 <bhavesh.aggarwal54@gmail.com>
2025-04-15 05:51:08 -0400

    favicon

--- components/AppMetadata.jsx
+++ components/AppMetadata.jsx
@@ -1,9 +1,9 @@
 const author = "Bhavesh Aggarwal";
 const description =
 	"Automation Engineer from Chandigarh, India, loves to develop and analyse Ideas and bring complex processes to automated workflows, Very keen to develop Scripts and RPA Workflows that can help in automating the processes and make Tedious repetitive tasks more efficient. I am a passionate developer with a strong interest in automation, and problem-solving. I enjoy creating innovative solutions that improve efficiency and productivity of a business.";
-const url = "https://vasile-novatchii.netlify.app";
+const url = "https://myportfolio-bhavesh25515.vercel.app/";
 export const AppMetadata = {
-	metadataBase: new URL("https://vasile-novatchii.netlify.app/"),
+	metadataBase: new URL("https://myportfolio-bhavesh25515.vercel.app/"),
 	title: {
 		default: `Portfolio | ${author}`,
 		template: `%s | ${author}`
@@ -30,20 +30,6 @@ export const AppMetadata = {
 		description: description,
 		url: url,
 		siteName: `${author} | Portfolio`,
-		images: [
-			{
-				url: "https://vasile-novatchii.netlify.app/screenshot.webp",
-				width: 800,
-				height: 600,
-				alt: "My personal portfolio website"
-			},
-			{
-				url: "https://vasile-novatchii.netlify.app/screenshot.webp",
-				width: 1800,
-				height: 1600,
-				alt: "My personal portfolio website"
-			}
-		],
 		locale: "en-US",
 		type: "website"
 	}
